7003a2d0cb91a8019fe1343dbe19f71b2bc8d59a
Hafthor <git@hafthor.net>
2023-09-22 09:12:24 -0400

    update mocha, fix minor bug

--- ugenpass.js
+++ ugenpass.js
@@ -4,11 +4,9 @@ const domain = process.argv[2] || process.exit(console.error('required argument 
 /^[a-z0-9][a-z0-9-.]*[a-z0-9]$/.test(domain) && !/[-.][-.]/.test(domain) || console.error('WARNING: Possibly invalid domain name given');
 process.stderr.write('domain: ' + domain + ', master password: ');
 process.stdin.setRawMode(true);
-require('readline').createInterface({input: process.stdin})
-    .on('close', process.exit)
-    .on('line', function(password) {
-        while (/\u007f/.test(password)) // process backspaces
-            password = password.replace(/[^\u007f]\u007f/, '').replace(/^\u007f+/, '');
-        console.error('[redacted]')
-        process.exit(process.stdout.write(ugenpass(password, domain)));
-    });
+require('readline').createInterface({input: process.stdin}).on('line', function(password) {
+    while (/\u007f/.test(password)) // process backspaces
+        password = password.replace(/[^\u007f]\u007f/, '').replace(/^\u007f+/, '');
+    console.error('[redacted]')
+    process.exit(process.stdout.write(ugenpass(password, domain)) ? 0 : 1);
+});
